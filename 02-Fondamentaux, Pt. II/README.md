25c6a6ce68d13f9a394f973920cd512f926be34f
DaFumery <tanydrej0@protonmail.com>
2022-07-11 14:23:07 +0200

    new

--- 02-Fondamentaux, Pt. II/README.md
+++ 02-Fondamentaux, Pt. II/README.md
@@ -211,3 +211,29 @@ console.log(mathys[4]); // [ 'Toto', 'Tati', 'Sullivan' ]
 // Accède à la valeur d'index0 du tableau ami intégré dans le tableau Mathys
 console.log(mathys[4][0]); // Toto
 ```
+
+#### Exercice tableaux
+
+Appliquer une fonction calcAge aux valeurs d'index[0], d'index[3] et à la dernière valeur du tableau annees.
+Créer un nouveau tableau ageReturn avec le résultat de la fonction calcAge()
+
+```js
+const calcAge = function (anneeNaissance) {
+  return 2022 - anneeNaissance;
+};
+
+const annees = [1997, 1998, 1999, 2000, 2001, 2002];
+
+const age1 = calcAge(annees[0]);
+const age2 = calcAge(annees[3]);
+const age3 = calcAge(annees[annees.length - 1]);
+console.log(age1, age2, age3); // 25 22 20
+
+const ageReturn = [
+  calcAge(annees[0]),
+  calcAge(annees[3]),
+  calcAge(annees[annees.length - 1]),
+];
+
+console.log(ageReturn); // [ 25, 22, 20 ]
+```
--- script.js
+++ script.js
@@ -1,28 +1,18 @@
-const prenom = "Mathys";
-const ami = ["Toto", "Tati", "Tata"];
-const annees = new Array(1997, 1996, 2001, 2002, 2008);
-const mathys = [prenom, "Fumery", 2022 - 1997, "développeur", ami];
+const calcAge = function (anneeNaissance) {
+  return 2022 - anneeNaissance;
+};
 
-console.log(ami); // [ 'Toto', 'Tati', 'Tata' ]
-console.log(annees); // [ 1997, 1996, 2001, 2002, 2008 ]
-console.log(mathys); // [ 'Mathys', 'Fumery', 25, 'développeur', [ 'Toto', 'Tati', 'Tata' ] ]
+const annees = [1997, 1998, 1999, 2000, 2001, 2002];
 
-// Renvoie la valeur d'index[0] et d'index[1]
-console.log(ami[0]); // "Toto"
-console.log(ami[1]); // "Tati"
+const age1 = calcAge(annees[0]);
+const age2 = calcAge(annees[3]);
+const age3 = calcAge(annees[annees.length - 1]);
+console.log(age1, age2, age3); // 25 22 20
 
-// Renvoie la longueur du tableau
-console.log(ami.length); // 3 (car 3 éléments)
+const ageReturn = [
+  calcAge(annees[0]),
+  calcAge(annees[3]),
+  calcAge(annees[annees.length - 1]),
+];
 
-// Renvoie la valeur d'index[3 - 1] = index[2]
-console.log(ami[ami.length - 1]); // Tata
-
-// Remplace la valeur d'index 2 du tableau ami
-ami[2] = "Sullivan";
-console.log(ami); // [ 'Toto', 'Tati', 'Sullivan' ]
-
-// Accède au tableau ami intégré dans le tableau mathys
-console.log(mathys[4]); // [ 'Toto', 'Tati', 'Sullivan' ]
-
-// Accède à la valeur d'index0 du tableau ami intégré dans le tableau Mathys
-console.log(mathys[4][0]); // Toto
+console.log(ageReturn);
