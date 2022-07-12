a8b6baf9af5444acdd1c08d4196102966f336d3e
DaFumery <tanydrej0@protonmail.com>
2022-07-12 13:08:42 +0200

    new

--- 03-DOM et Events Fondamentaux/Modal/script.js
+++ 03-DOM et Events Fondamentaux/Modal/script.js
@@ -1,1 +1,45 @@
 'use strict';
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+/// Variables
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+/// Expression de functions
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+/// Event Handler on Click
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+/// Event Handler on ESC
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// !modal.classList.contains('hidden')
+// Si modal ne CONTIENT PAS la classe 'Hidden' alors...
