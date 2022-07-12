009747853512ba2e2a446659ae8550531cce1a18
DaFumery <tanydrej0@protonmail.com>
2022-07-12 10:32:14 +0200

    new

--- 03-DOM et Events Fondamentaux/JackLeRoiDesChiffres/script.js
+++ 03-DOM et Events Fondamentaux/JackLeRoiDesChiffres/script.js
@@ -1,16 +1,66 @@
 'use strict';
 
-const nombre = Math.trunc(Math.random() * 20) + 1;
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+/// Variables
+let score = 20;
 
-console.log(Math.sqrt(25)); // 5
-console.log(Math.sqrt(64)); // 8
-console.log(Math.sqrt(69)); // 8.30662386
-console.log(Math.sqrt(-420.69)); // NaN
+// Génère un nombre aléatoire entre 0 et 20
+const randomNombre = Math.trunc(Math.random() * 20) + 1;
 
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+/// Selectors
+document.querySelector('.nombre').textContent = randomNombre;
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+/// Events
 document.querySelector('.verifier').addEventListener('click', function () {
-  console.log(document.querySelector('.deviner').value);
+  const deviner = Number(document.querySelector('.deviner').value);
+  console.log(deviner, typeof deviner);
 
+  // No input
   if (!deviner) {
-    document.querySelector('.message').textContent = '❓';
+    document.querySelector('.message').textContent = 'Pas de nombre ❗';
+  }
+
+  // Situation de victoire
+  else if (deviner === randomNombre) {
+    document.querySelector('.message').textContent = "(●'◡'●) Correcte !";
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.nombre').style.width = '30rem';
+  }
+
+  // Situation de nombre trop haut
+  else if (deviner > randomNombre) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Trop haut !';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+
+    // Situation de défaite
+    else {
+      document.querySelector('.message').textContent = '🧨 Vous avez perdu';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#60b347';
+    }
+  }
+
+  // Situation de nombre trop bas
+  else if (deviner < randomNombre) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Trop bas !';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+
+    // Situation de défaite
+    else {
+      document.querySelector('.message').textContent = '🧨 Vous avez perdu';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#60b347';
+    }
   }
 });
